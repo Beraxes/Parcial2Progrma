50592eb3949da772ae3705a22c798c025293a86e
Javier Andres Olivera Soto <javierandresoliverasoto@gmail.com>
2022-06-25 08:19:28 -0500

    commmit inicial del parcial 2 programacion

---
+++ src/elecciones/sufragante.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elecciones;
+
+/**
+ *
+ * @author beraxes
+ */
+public class sufragante {
+
+    int cedula;
+    String nombre;
+    int voto;
+    sufragante enlace;
+
+    public sufragante() {
+        cedula = 0;
+        nombre = "";
+        voto = 0;
+        enlace = null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public sufragante getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(sufragante enlace) {
+        this.enlace = enlace;
+    }
+
+    public int getVoto() {
+        return voto;
+    }
+
+    public void setVoto(int voto) {
+        this.voto = voto;
+    }
+
+    public void registrarSufr(int ce, String nomb, int vo) {
+        setCedula(ce);
+        setNombre(nomb);
+        setVoto(vo);
+    }
+
+}
